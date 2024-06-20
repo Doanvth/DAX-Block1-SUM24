9a923c2698c703f1b3c71601154777913a5d0013
icyRick <trongtri.1975s@gmail.com>
2024-06-20 18:09:52 +0700

    [AngularJS] - Thêm mới controller 'bottleCtrl'

--- js/app.js
+++ js/app.js
@@ -6,6 +6,20 @@ app.config(function ($routeProvider) {
       templateUrl: "trangchu.html",
       controller: "homeController"
     })
+    .when("/bottle", {
+      templateUrl: "bottle.html",
+      controller: "bottleCtrl"
+    })
 });
+let URL_API = "http://localhost:3000/";
 
-let URL_API = "http://localhost:3000/" 
+app.controller("bottleCtrl", function ($scope, $http) {
+  $scope.getBottleData = function () {
+    $http.get(URL_API + "bottle").then(function (success) {
+      $scope.bottles = success.data;
+    }, function (error) {
+      console.log("Error: ", error);
+    }
+    )
+  }
+})
