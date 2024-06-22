09e327dfd65e107216debeed14d6f1e3e77c3e72
icyRick <trongtri.1975s@gmail.com>
2024-06-22 18:01:49 +0700

    [AngularJS] - fill data page bottle

--- js/app.js
+++ js/app.js
@@ -1,25 +1,48 @@
 var app = angular.module("myApp", ["ngRoute"]);
 
 app.config(function ($routeProvider) {
-  $routeProvider
+    $routeProvider
     .when("/", {
-      templateUrl: "trangchu.html",
-      controller: "homeController"
+        templateUrl: "trangchu.html",
+        controller: "homeController",
     })
-    .when("/bottle", {
+    .when("/bottle/", {
       templateUrl: "bottle.html",
       controller: "bottleCtrl"
     })
+    .when("/bottle/:id", {
+      templateUrl: "bottle.html",
+      controller: "bottleCtrl"
+    });
 });
 let URL_API = "http://localhost:3000/";
 
 app.controller("bottleCtrl", function ($scope, $http) {
-  $scope.getBottleData = function () {
-    $http.get(URL_API + "bottle").then(function (success) {
-      $scope.bottles = success.data;
-    }, function (error) {
-      console.log("Error: ", error);
-    }
-    )
-  }
-})
+    $scope.getBottleData = function () {
+        $http.get(URL_API + "bottles").then(
+            function (success) {
+                $scope.bottles = success.data;
+
+                $scope.bottleBrands = $scope.bottles.brands;
+                $scope.bottleBudgets = $scope.bottles.budgets;
+                $scope.bottleCapacities = $scope.bottles.capacities;
+                $scope.bottleMadeAt = $scope.bottles.made_at;
+                $scope.bottleProducts = $scope.bottles.products;
+                $scope.bottleHotCategories = $scope.bottles.hot_categories;
+                $scope.bottleCategories = $scope.bottles.categories;
+                $scope.extractCategories();
+                console.log($scope.bottleCategories);
+                console.log($scope.subcategoriesList);
+                console.log($scope.subcategoriesListSmall);
+            },
+            function (error) {
+                console.log("Error: ", error);
+            }
+        );
+    };
+    $scope.showMore = false;
+    $scope.getBottleData();
+    $scope.showMoreItem = function (show) {
+        $scope.showHiddenItem = show;
+    };
+});
