7505ab3c629cda60ed2505a78ea3d64aadbbcee3
DangLH <ledang01637@gmail.com>
2024-06-24 08:35:48 +0700

    [AngularJS]-DetaiLVS

--- js/app.js
+++ js/app.js
@@ -18,9 +18,17 @@ app.config(function ($routeProvider) {
       templateUrl: "noi.html",
       controller: "noiController"
     })
-    .when("/detailNoi/:id",{
+    .when("/detailNoi/:id", {
       templateUrl: "detailNoi.html",
       controller: "detailNoiController"
+    })
+    .when("/lovisong", {
+      templateUrl: "lovisong.html",
+      controller: "lovisongController"
+    })
+    .when("/detailLovisong/:id", {
+      templateUrl: "detailLovisong.html",
+      controller: "detailLovisongController"
     })
 });
 let URL_API = "http://localhost:3000/"
@@ -218,53 +226,65 @@ app.directive('quantityControl', function () {
   };
 });
 
-app.service('NoiService', function($http) {
-  this.getCategoriesNoi = function() {
+app.service('NoiService', function ($http) {
+  this.getCategoriesNoi = function () {
     return $http({
       method: "GET",
       url: URL_API + "danhmucnoi",
-    }).catch(function(error) {
+    }).catch(function (error) {
       console.error('API call error:', error);
-      throw error; 
+      throw error;
     });
   };
 });
 
-app.service('BrandService', function($http) {
-  this.getBrandNoi = function() {
+app.service('BrandService', function ($http) {
+  this.getBrandNoi = function () {
     return $http({
       method: "GET",
       url: URL_API + "brandNoi",
-    }).catch(function(error) {
+    }).catch(function (error) {
       console.error('API call error:', error);
-      throw error; 
+      throw error;
     });
   };
 });
 
-app.service('madeInNoiService', function($http) {
-  this.getMadeInNoi = function() {
+app.service('madeInNoiService', function ($http) {
+  this.getMadeInNoi = function () {
     return $http({
       method: "GET",
       url: URL_API + "madeInNoi",
-    }).catch(function(error) {
+    }).catch(function (error) {
       console.error('API call error:', error);
-      throw error; 
+      throw error;
     });
   };
 });
 
-app.controller('noiController', function($scope, NoiService, BrandService, madeInNoiService) {
-  NoiService.getCategoriesNoi().then(function(response) {
+app.service('LovisongService', function ($http) {
+  this.getLovisong = function () {
+    return $http({
+      method: "GET",
+      url: URL_API + "LoViSong",
+    }).catch(function (error) {
+      console.error('API call error:', error);
+      throw error;
+    });
+  };
+});
+
+app.controller('noiController', function ($scope, NoiService, BrandService, madeInNoiService) {
+  NoiService.getCategoriesNoi().then(function (response) {
     $scope.categories = response.data;
     $scope.productsByCategory = {};
 
     function processCategories(categories, parentId) {
-      categories.forEach(function(category) {
+      categories.forEach(function (category) {
         if (category.potType) {
-          category.potType.forEach(function(subCategory) {
+          category.potType.forEach(function (subCategory) {
             if (subCategory.products) {
-              subCategory.products.forEach(function(product) {
+              subCategory.products.forEach(function (product) {
                 $scope.productsByCategory[product.name] = {
                   product: product,
                   categoryId: parentId
@@ -276,48 +296,106 @@ app.controller('noiController', function($scope, NoiService, BrandService, madeI
       });
     }
     processCategories($scope.categories, null);
-  }).catch(function(error) {
+  }).catch(function (error) {
     $scope.error = 'Error occurred: ' + error.message;
     console.error('Error occurred:', error);
   });
-  BrandService.getBrandNoi().then(function(response){
+  BrandService.getBrandNoi().then(function (response) {
     $scope.brand = response.data;
   });
-  madeInNoiService.getMadeInNoi().then(function(response){
+  madeInNoiService.getMadeInNoi().then(function (response) {
     $scope.madeIn = response.data;
   });
 });
 
-app.controller('detailNoiController', function($scope, $http, $routeParams, $location) {
+app.controller('detailNoiController', function ($scope, $http, $routeParams, $location) {
   $http({
     method: "GET",
     url: URL_API + "danhmucnoi"
-}).then(function(response) {
+  }).then(function (response) {
     var danhMucNoi = response.data;
 
     if (Array.isArray(danhMucNoi)) {
-        danhMucNoi.forEach(function(danhmuc) {
-            var potType = danhmuc.potType;
-            if (Array.isArray(potType)) {
-                potType.forEach(function(type) {
-                    var products = type.products;
-                    if (Array.isArray(products)) {
-                        products.forEach(function(product) {
-                            if (product.id == $routeParams.id) {
-                                $scope.product = product;
-                                console.log($scope.product);
-                            }
-                        });
-                    }
-                });
+      danhMucNoi.forEach(function (danhmuc) {
+        var potType = danhmuc.potType;
+        if (Array.isArray(potType)) {
+          potType.forEach(function (type) {
+            var products = type.products;
+            if (Array.isArray(products)) {
+              products.forEach(function (product) {
+                if (product.id == $routeParams.id) {
+                  $scope.product = product;
+                }
+              });
             }
-        });
+          });
+        }
+      });
     } else {
-        console.log("Dữ liệu danh mục nồi không hợp lệ.");
+      console.log("Dữ liệu danh mục nồi không hợp lệ.");
     }
 
-}).catch(function(error) {
+  }).catch(function (error) {
     console.log("Lỗi khi tải danh mục nồi:", error);
+  });
+
 });
+
+app.controller('lovisongController', function ($scope, LovisongService) {
+  LovisongService.getLovisong().then(function (response) {
+    $scope.categories = response.data;
+    $scope.productsByCategory = {};
+
+    function processCategories(categories, parentId) {
+      categories.forEach(function (products) {
+        $scope.products = products.Product;
+      });
+      categories.forEach(function (categori) {
+        if (categori.CatagoryLoViSong) {
+          categori.CatagoryLoViSong.forEach(function (inforLVS) {
+            $scope.madeInLVS = inforLVS.Production;
+            $scope.brandLVS = inforLVS.Brand;
+            $scope.priceLVS = inforLVS.Price;
+            $scope.cateLVS = inforLVS.Categories;
+          })
+        }
+
+
+
+
+      })
+    }
+    processCategories($scope.categories, null);
+  }).catch(function (error) {
+    $scope.error = 'Error occurred: ' + error.message;
+    console.error('Error occurred:', error);
+  });
+});
+
+app.controller('detailLovisongController', function ($scope, $http, $routeParams, $location) {
+  $http({
+    method: "GET",
+    url: URL_API + "LoViSong"
+  }).then(function (response) {
+    var data  = response.data;
+    if (Array.isArray(data)) {
+      data.forEach(function (item) {
+        if (item.Product && Array.isArray(item.Product)) {
+          var products = item.Product;
+          products.forEach(function(product){
+            if(product.id == $routeParams.id){
+              $scope.product = product;
+              console.log($scope.product)
+            }
+          })
+        }
+      });
+    }
+    
+
+
+  }).catch(function (error) {
+    console.log("Lỗi khi tải danh mục lò vi sóng:", error);
+  });
 
 });
